fa1ceca7e1517959e34efc5d596492055ed7979d
igorcardonaalves <igoralves@edu.unisinos.br>
2021-03-17 13:44:52 +0044

    Update Jogo.h

--- jornalunicornio/Jogo.h
+++ jornalunicornio/Jogo.h
@@ -1,15 +1,21 @@
 #pragma once
 #include "libUnicornio.h"
 
+
 class Jogo
 {
 public:
 	Jogo();
 	~Jogo();
-
 	void inicializar();
 	void finalizar();
-
 	void executar();
+protected:
+	void carregarRecursos();
+private:
+	Jornal jornal;
+	Sprite sprite_tempJornal, player;
+	Texto texto_tituloJornal, texto_coluna;
+	
 };
 
