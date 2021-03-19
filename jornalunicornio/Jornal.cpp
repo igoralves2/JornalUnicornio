9aded2fa3892fb5da7b904976998bd3788e443b5
igorcardonaalves <igoralves@edu.unisinos.br>
2021-03-19 16:33:31 +0044

    Update Jornal.cpp

--- jornalunicornio/Jornal.cpp
+++ jornalunicornio/Jornal.cpp
@@ -4,12 +4,15 @@ Jornal::Jornal() {
 
 }
 
-Jornal::Jornal(string novotitulo, string novadataedicao, string novotitulonoticia, string novotextonoticia)
+Jornal::Jornal(string novotitulo, string novadataedicao, int novonroColunas)
 {
 	titulo = novotitulo;
 	dataedicao = novadataedicao;
-	titulonoticia = novotitulonoticia;
-	textonoticia = novotextonoticia;
+	nroNoticias = 0;
+	nroPaginas = 0;
+	nroPaginas = 0;
+	nroColunas = novonroColunas;
+	paginaatual = 0;
 }
 
 Jornal::~Jornal()
@@ -26,12 +29,80 @@ string Jornal::getDataEdicao()
 	return dataedicao;
 }
 
-string Jornal::getTituloNoticia()
+Noticia Jornal::getNoticia(int index)
 {
-	return titulonoticia;
+	if (nroNoticias == 0) {
+		return Noticia("Erro", "Isto não é uma notícia");
+	}if (index < 0 || index >= nroNoticias) {
+		return Noticia("Erro", "Isto não é uma notícia");
+	}return noticias[index];
 }
 
-string Jornal::getTextoNoticia()
+Pagina Jornal::getPagina(int index)
 {
-	return textonoticia;
+	if (nroPaginas == 0) {
+		return Pagina(0);
+	}if (index < 0 || index >= nroPaginas) {
+		return Pagina(0);
+	}return paginas[index];
+}
+
+
+void Jornal::setTitulo(string novo) {
+	titulo = novo;
+}
+
+void Jornal::setDataEdicao(string nova)
+{
+	dataedicao = nova;
+}
+
+void Jornal::adicionarImagemNoticia(int index, string novatextura)
+{
+	getNoticia(index).adicionarTextura(novatextura);
+}
+		
+void Jornal::adicionarMaisColunasNoticia(int nro)
+{
+	nroColunas += nro;
+}
+
+void Jornal::adicionarNoticia(Noticia nova)
+{
+	if (nroNoticias >= maxnoticias) {
+		return;
+	}
+	noticias[nroNoticias] = nova;
+	nroNoticias;
+}
+
+void Jornal::adicionarPagina(Pagina p)
+{
+	if (nroPaginas >= maxpaginas) {
+		return;
+	}
+	paginas[nroPaginas] = p;
+	nroPaginas++;
+}
+
+int Jornal::getNroColunas()
+{
+	return nroColunas;
+}
+
+int Jornal::getPaginaAtual()
+{
+	return paginaatual;
+}
+
+void Jornal::alternapagina(bool avanca)
+{
+	if (nroPaginas > 0) {
+		if (avanca) {
+			paginaatual = (paginaatual + 1) % nroPaginas;
+		}
+		else {
+			paginaatual = (paginaatual - 1) % nroPaginas;
+		}
+	}
 }
