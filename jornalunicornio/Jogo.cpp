d0afe47b74bb437e4719ce6ffab46a797d3b1cd6
igorcardonaalves <igoralves@edu.unisinos.br>
2021-03-19 16:33:25 +0044

    Update Jogo.cpp

--- jornalunicornio/Jogo.cpp
+++ jornalunicornio/Jogo.cpp
@@ -41,11 +41,31 @@ void Jogo::inicializar()
 	
 	player.setSpriteSheet("player");
 	player.setAnimacao(0);
-	
+
 	//Jornal
 
-	jornal = Jornal(texto_tituloJornal.getString(), "17/03/2021", "Games All-Stars", texto_coluna.getString());
+	jornal = Jornal(texto_tituloJornal.getString(), "17/03/2021", 10);
 
+	//Adicionar mais noticias
+
+	jornal.adicionarNoticia(Noticia("Gaming All-Stars", texto_coluna.getString()));
+
+	//Adicionar imagem
+
+	jornal.adicionarImagemNoticia(0, "imagem1");
+
+	//Adicionar colunas
+
+	jornal.adicionarMaisColunasNoticia(5);
+
+	//Adicionar mais páginas
+
+	jornal.adicionarPagina(Pagina(0));
+	jornal.adicionarPagina(Pagina(1));
+	jornal.adicionarPagina(Pagina(2));
+	jornal.getPagina(0).setTexto(texto_coluna.getString());
+	jornal.getPagina(1).setTexto("Escreva o que você quer colocar na página 2:");
+	jornal.getPagina(2).setTexto("Escreva o que você quer colocar na página 3:");
 	//Painel Debug
 	gDebug.setPos(150, 350);
 }
@@ -68,9 +88,32 @@ void Jogo::executar()
 			gDebug.restaurar();
 		//	Seu código vem aqui!
 		//	...
-		sprite_tempJornal.desenhar(gJanela.getLargura() / 2, gJanela.getAltura() /1.2);
+		if (gTeclado.pressionou[TECLA_X]) {
+			jornal.alternapagina(true);
+			texto_coluna.setFonte("TextoColuna");
+			texto_coluna.setString(jornal.getPagina(jornal.getPaginaAtual()).getTexto());
+			texto_coluna.setAlinhamento(TEXTO_ALINHADO_A_ESQUERDA);
+			texto_coluna.setLarguraMaxima(100);
+			texto_coluna.setEscala(0.5, 0.5);
+			texto_coluna.setCor(0, 0, 0);
+			texto_coluna.setEspacamentoLinhas(1.5);
+		}
+		else if (gTeclado.pressionou[TECLA_Z]) {
+			jornal.alternapagina(false);
+			texto_coluna.setFonte("TextoColuna");
+			texto_coluna.setString(jornal.getPagina(jornal.getPaginaAtual()).getTexto());
+			texto_coluna.setAlinhamento(TEXTO_ALINHADO_A_ESQUERDA);
+			texto_coluna.setLarguraMaxima(100);
+			texto_coluna.setEscala(0.5, 0.5);
+			texto_coluna.setCor(0, 0, 0);
+			texto_coluna.setEspacamentoLinhas(1.5);
+		}sprite_tempJornal.desenhar(gJanela.getLargura() / 2, gJanela.getAltura() / 1.2);
 		texto_tituloJornal.desenhar(gJanela.getLargura() / 2, gJanela.getAltura() / 6);
-		texto_coluna.desenhar(gJanela.getLargura() / 3.8, gJanela.getAltura() / 1.8);
+		texto_coluna.desenhar(gJanela.getLargura() / jornal.getNroColunas(), gJanela.getAltura() / 1.8);
+		/*
+		texto_tituloJornal.desenhar(gJanela.getLargura() / 2, gJanela.getAltura() / 6);
+		texto_coluna.desenhar(gJanela.getLargura() / jornal.getNroColunas(), gJanela.getAltura() / 1.8);
+		*/
 		gDebug.depurar("FPS", (int)gTempo.getFPS());
 		gDebug.depurar("deltaTempo", gTempo.getDeltaTempo());
 		player.avancarAnimacao();
@@ -85,4 +128,7 @@ void Jogo::carregarRecursos()
 	gRecursos.carregarFonte("TextoColuna", "../assets/new_browserlink.ttf");
 	gRecursos.carregarSpriteSheet("templateJornal", "../assets/JornalTemplate.png", 1, 1);
 	gRecursos.carregarSpriteSheet("player", "../assets/Player.png", 1, 2);
+	gRecursos.carregarTextura("imagem1", "../assets/th.png");
+	gRecursos.carregarTextura("imagem2", "../assets/switchnews.png");
+	gRecursos.carregarTextura("imagem3", "../assets/th.png");
 }
