ffc23cd74fea1db2a3f7a57d7b1f0e68184d9bf6
igorcardonaalves <igoralves@edu.unisinos.br>
2021-03-19 16:33:39 +0044

    Update Jornal.h

--- jornalunicornio/Jornal.h
+++ jornalunicornio/Jornal.h
@@ -1,20 +1,38 @@
 #pragma once
 #include "libUnicornio.h"
 #include <string>
+#include "Noticia.h"
+#include "Pagina.h"
 class Jornal
 {
 public:
 	Jornal();
-	Jornal(string novotitulo, string novadataedicao, string novotitulonoticia, string novotextonoticia);
+	Jornal(string novotitulo, string novadataedicao, int novonroColunas);
 	~Jornal();
 	string getTitulo();
 	string getDataEdicao();
-	string getTituloNoticia();
-	string getTextoNoticia();
+	Noticia getNoticia(int index);
+	Pagina getPagina(int index);
+	void setTitulo(string novo);
+	void setDataEdicao(string nova);
+	void adicionarImagemNoticia(int index, string textura);
+	void adicionarMaisColunasNoticia(int nro);
+	void adicionarNoticia(Noticia nova);
+	void adicionarPagina(Pagina p);
+	int getNroColunas();
+	int getPaginaAtual();
+	void alternapagina(bool avanca);
+	int maxpaginas = 10;
+	int maxnoticias = 10;
 protected:
 	string titulo;
 	string dataedicao;
-	string titulonoticia;
-	string textonoticia;
+	Noticia noticias[10];
+	Pagina paginas[10];
+	int index;
+	int nroColunas;
+	int nroNoticias;
+	int nroPaginas;
+	int paginaatual;
 };
 
